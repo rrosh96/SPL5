06e085b2682f845969ed5cc9ea84777b8c76f614
roshanraju <rroshraju@gmail.com>
2018-09-07 13:24:00 +0530

    BoilerPlate Code

--- bot.js
+++ bot.js
@@ -63,14 +63,105 @@ class Bot {
         console.log("Game over ::", data.result);
     }
 
+
+    findBotPositions(idArray) {
+        let self = this;
+        let boardInfo = this.request.boardInfo;
+        let temp = {};
+        idArray.forEach(function(id){
+            temp[id] = [];
+            let size = boardInfo.length;
+            for(let i = 0; i < size; i++){
+                for(let j = 0; j < size; j++){
+                    if(id === boardInfo[i][j]){
+                        temp[id].push([i,j]);
+                    }
+                }
+            }
+        })
+        
+        return temp;
+    }
+
+    // Removes all positions that are out of boundary
+    filterOutOfBoundaryMoves (positions) {
+        let size = this.request.boardInfo.length;
+        return positions.filter(function (pos) {
+            return ((pos[0] >= 0 && pos[0] < size) && (pos[1] >= 0 && pos[1] < size))
+        })
+    }
+
+    // Removes all position with the specific id
+    filterById (positions, id) {
+        let boardInfo = this.request.boardInfo;
+        return positions.filter(function (pos) {
+            let i = pos[0],
+            j = pos[1];
+            return id.indexOf(boardInfo[i][j]) === -1;
+        });
+    }
+
+    //Returns all position relative to cellPos and step from cellPos
+    findAllPositions (cellPos, step) {
+        return [
+            [cellPos[0] - step, cellPos[1] - step],
+            [cellPos[0] - step, cellPos[1]],
+            [cellPos[0] - step, cellPos[1] + step],
+            [cellPos[0], cellPos[1] - step],
+            [cellPos[0], cellPos[1] + step],
+            [cellPos[0] + step, cellPos[1] - step],
+            [cellPos[0] + step, cellPos[1]],
+            [cellPos[0] + step, cellPos[1] + step]
+        ]
+    }
+
+    //Returns only the possible positions from the cellPos
+    findOnlyPossibePositions (cellPos, step) {
+        // getAll possible moves 
+        let allPos = this.findAllPositions(cellPos, step);
+
+        let filter1 = this.filterOutOfBoundaryMoves(allPos);
+        
+        // filter by both opp bot id and block id
+        let filter2 = this.filterById(filter1, [2, -1]);
+        return filter2;
+    }
+
+    findPossibleMoves (cellPos, step) {
+        return this.findOnlyPossibePositions(cellPos, step);
+    }
+
+
+
+
+
+    
+    // takes request as param
+    // return complete respons object 
+    whatsMyMove(){
+        // assign variable to freq used params, myID, oppID 
+        let myID = this.request.yourID;
+        let oppID = 3 % myID;
+        // return object  of Bot Position for the corresponding ID 
+        // obj = {myID : [], oppID : []}
+        this.findBotPositions([1,2]);
+
+        // // cellPosition can be my cell or opponent Cell
+        // // step = 1 for clone, step =2 for jump
+        let moves = this.findPossibleMoves([6,0], 1) 
+
+        // // add Decision making logic here 
+        this.wife();
+    }
+    
     move(request) {
         console.log("Request move:", request.boardInfo);
-
+        this.request = request;
         //
         // ** Move logic here. **
         //
 
-        
+        this.whatsMyMove();
         
         let response = {
             dataType: "response",
@@ -81,23 +172,8 @@ class Bot {
         console.log("Respond move:", response);
         this.splClient.respond(response);
     }
-
-    // takes request as param
-    // return complete respons object 
-    whatsMyMove(request){
-        // assign variable to freq used params, myID, oppID 
-
-        // return object  of Bot Position for the corresponding ID 
-        // obj = {myID : [], oppID : []}
-        this.findBotPositions(idArray);
-
-        // cellPosition can be my cell or opponent Cell
-        // step = 1 for clone, step =2 for jump
-        this.findPossibleMoves(cellPos, step) 
-
-        // add Decision making logic here 
-        this.wife();
-    }
+    
+    
 }
 
 module.exports = Bot;
